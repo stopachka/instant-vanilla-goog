280d32b240d48f0cb4a20fa8170161377ea4e435
stopachka <stepan.p@gmail.com>
2024-06-04 13:27:01 -0400

    goog example

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,88 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import { User, id, init } from "@instantdb/core";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+const db = init({
+  appId: "5b640925-7328-451d-b79e-a7d4c8b48adb",
+});
+
+const appEl = document.querySelector<HTMLDivElement>("#app")!;
+
+function renderLoading() {
+  appEl.innerHTML = `<div>Loading...</div>`;
+}
+
+function renderAuthError(message: string) {
+  appEl.innerHTML = `<div>Uh oh! ${message}</div>`;
+}
+
+function renderLoggedInPage(user: User) {
+  appEl.innerHTML = `
+    <div>
+      <h1>Welcome, ${user.email}!</h1>
+      <button id='sign-out-button'>Sign out</button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+  `;
+  const signOutBtn =
+    document.querySelector<HTMLButtonElement>("#sign-out-button")!;
+  signOutBtn.addEventListener("click", () => {
+    db.auth.signOut();
+  });
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function renderSignInPage() {
+  const googAuthURI = db.auth.createAuthorizationURL({
+    clientName: "google-web",
+    redirectURL: window.location.href,
+  });
+  appEl.innerHTML = `
+    <div>
+      <form id='email-input-form'>
+        <h3>Welcome to log in with Google!</h3>
+        <p>
+          You have two options:
+        </p>
+        <p> 
+          1. You can click <a href="${googAuthURI}">here</a> to sign in with Google.
+        </p>  
+        <p> 
+         Or, you can use the Google One-Tap button:
+        </p>
+        <div id="goog-button"></div> 
+    </div>
+  `;
+  const googBtn = document.getElementById("goog-button")!;
+  const nonce = id();
+  window.google.accounts.id.initialize({
+    client_id:
+      "873926401300-t33oit5b8j5n0gl1nkk9fee6lvuiaia0.apps.googleusercontent.com",
+    callback: (response) => {
+      db.auth.signInWithIdToken({
+        idToken: response.credential,
+        clientName: "google-web",
+        nonce: nonce,
+      });
+    },
+    nonce: nonce,
+  });
+  window.google.accounts.id.renderButton(
+    googBtn,
+    {
+      theme: "outline",
+      size: "large",
+      type: "standard",
+      width: 300,
+    },
+  );
+}
+
+
+renderLoading();
+db.subscribeAuth((auth) => {
+  if (auth.error) {
+    renderAuthError(auth.error.message);
+  } else if (auth.user) {
+    renderLoggedInPage(auth.user);
+  } else {
+    renderSignInPage();
+  }
+});
